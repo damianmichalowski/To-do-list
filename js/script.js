a4df74e8f5d65a413be3a15225484702429ca240
Damian Michalowski <dowski888@gmail.com>
2022-02-28 16:21:42 +0100

    tasks adding

--- js/script.js
+++ js/script.js
@@ -14,10 +14,12 @@
     const render = () => {
         let htmlString = "";
 
-        for(const task of tasks){
+        for (const task of tasks) {
             htmlString += `
-                <li>
-                ${task.content}
+                <li 
+                    ${task.done ? "style=\"text-decoration: line-through\"" : ""}
+                >
+                    ${task.content}
                 </li>
             `;
         }
@@ -25,8 +27,31 @@
         document.querySelector(".js-tasks").innerHTML = htmlString;
     };
 
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+
+        const newTaskContent = document.querySelector(".js-newTask").value.trim();
+
+        if (newTaskContent === "") {
+            return;
+        }
+
+        addNewTask(newTaskContent);
+    };
+
+    const addNewTask = (newTaskContent) => {
+        
+        tasks.push({
+            content: newTaskContent,
+        });
+
+        render();
+    };
+
     const init = () => {
         render();
+        const form = document.querySelector(".js-form");
+        form.addEventListener("submit", onFormSubmit);
     };
 
     init();
